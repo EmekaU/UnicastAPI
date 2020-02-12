826ecef4b4313ec6507bf728f3c5abb6c0cf03e0
Valentine Umeozo <varllyy@gmail.com>
2020-02-12 14:31:52 -0600

    Initial Commit

---
+++ src/main/java/com/group/unicast/UnicastApplication.java
@@ -0,0 +1,13 @@
+package com.group.unicast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnicastApplication.class, args);
+    }
+
+}
