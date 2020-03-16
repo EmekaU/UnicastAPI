fbf0c11096024ea4a7076666cd26eb669a363839
Umeozo <cvu302@usask.ca>
2020-03-15 21:08:31 -0600

    Unicast

--- src/main/java/com/group/repository/UserRepo.java
+++ src/main/java/com/group/repository/UserRepo.java
@@ -4,9 +4,15 @@ import com.group.dao.UserDao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepo extends JpaRepository<UserDao, String> {
 
     public boolean existsByUsername(String username);
+
+    public boolean deleteUserDaoByUsername(String username);
+
+    // search for users with username containing x, y, z
 
 }
